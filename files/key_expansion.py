e7ed2d6b9ad55e815f23e630412f4f4e25f1adbe
mdskrzypczyk <mdskrzypczyk@skippy>
2015-05-11 15:33:42 -0500

    Work on key expansion

--- files/key_expansion.py
+++ files/key_expansion.py
@@ -1,3 +1,21 @@
-class KeyExpansion:
+sbox = open('sbox').read().replace('\n',' ').split(' ')
+rcon_box = open('rcon').read().replace('\n', '').split()
+def rot_word(word):
+    return (word[2:]+word[0:1])
+
+def sub_word(word):
+    for _ in range(len(word)/2):
+        word.replace(word[_:_+2], sbox[int(word[_],16)*16 + int(word[_+1])])
+
+    return word
+
+def rcon(val):
+
+
+def ek():
+
+def k():
+
+KeyExpansion:
     def __init__(self, key, key_size):
         self.expanded_key
