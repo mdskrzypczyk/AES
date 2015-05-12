0fed9ec70afb2cd76490753db3e4c3b9265008a0
mdskrzypczyk <mdskrzypczyk@gmail.com>
2015-05-11 19:07:35 -0500

    Key Expansion works!

--- files/key_expansion.py
+++ files/key_expansion.py
@@ -1,21 +1,52 @@
-sbox = open('sbox').read().replace('\n',' ').split(' ')
-rcon_box = open('rcon').read().replace('\n', '').split()
+sbox = open('files/sbox').read().replace('\n',' ').split(' ')
+rcon_box = open('files/rcon').read().split('\n')
+
 def rot_word(word):
-    return (word[2:]+word[0:1])
+    return (word[2:]+word[0:2])
 
 def sub_word(word):
-    for _ in range(len(word)/2):
-        word.replace(word[_:_+2], sbox[int(word[_],16)*16 + int(word[_+1])])
+    new_word = ''
+    for _ in range(0,len(word),2):
+        new_word += sbox[int(word[_],16)*16 + int(word[_+1],16)]
 
-    return word
+    return new_word
 
 def rcon(val):
+    return rcon_box[val]
 
+def ek(expanded_key, offset):
+    byte_offset = offset*2
+    return expanded_key[byte_offset:byte_offset+8]
 
-def ek():
-
-def k():
-
-KeyExpansion:
+class KeyExpansion:
     def __init__(self, key, key_size):
-        self.expanded_key
+        self.expanded_key = ''
+        self.expanded_key += key
+
+        if key_size == '16':
+            start_round = 4
+            rounds = 44
+        elif key_size == '24':
+            start_round = 6
+            rounds = 52
+        elif key_size == '32':
+            start_round = 8
+            rounds = 60
+        
+        for expansion_round in range(start_round, rounds):
+            if (expansion_round % 4) == 0:
+                first_ek = ek(self.expanded_key, (expansion_round-1)*4)
+                first_ek = sub_word(rot_word(first_ek))
+                r_con = rcon((expansion_round/4)-1)
+                second_ek = ek(self.expanded_key, (expansion_round-4)*4)
+                arg = [int(a,16) ^ int(b,16) ^ int(c,16) for a,b,c in zip(first_ek,r_con,second_ek)]
+                arg = ''.join([hex(a).replace('0x','') for a in arg]).upper()
+                self.expanded_key += arg
+
+            else:
+                first_ek = ek(self.expanded_key, (expansion_round-1)*4)
+                second_ek = ek(self.expanded_key, (expansion_round-4)*4)
+                arg = [int(a,16) ^ int(b,16) for a,b in zip(first_ek,second_ek)]
+                arg = ''.join([hex(a).replace('0x','') for a in arg]).upper()
+                self.expanded_key += arg
+
--- run.py
+++ run.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python
 from sys import argv
-from files import encryption, decryption, key_expansion
+from files import key_expansion
 import os
 
-script, operation, block, key_size = argv
-
-if operation == 'encrypt':
-    key = os.urandom(key_size)
-    expanded_key = key_expansion.KeyExpansion(key,key_size).expanded_key
-    pass
-elif operation == 'decrypt':
-    pass
-else:
-    print 'This program is designed to be used in the following:'
-    print 'run.py [operation] [block] [key_size]'
-    print 'operation: encrypt or decrypt'
-    print 'block: block to perform operation on'
-    print 'key_size: the key size to use'
+script, key, key_size = argv
+generated_key = key_expansion.KeyExpansion(key,key_size).expanded_key
