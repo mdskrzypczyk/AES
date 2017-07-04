15aa2908275f01e7a11977206d6b25c2c60bbec4
mdskrzypczyk <mdskrzypczyk@gmail.com>
2017-07-03 21:18:22 -0500

    Saving state

--- tests/unit/test_decryption_primitives.py
+++ tests/unit/test_decryption_primitives.py
@@ -1,11 +1,18 @@
 import unittest
+from aes.aes_lib import ibyte_sub, ishift_row, imix_column
 
 class TestDecryptionPrimitives(unittest.TestCase):
 	def test_inverse_byte_sub(self):
-		pass
+		data = [99, 124, 119, 123, 242, 107, 111, 197, 48, 1, 103, 43, 254, 215, 171, 118]
+		res = ibyte_sub(data)
+		assert res == range(16)
 
 	def test_inverse_shift_row(self):
-		pass
+		data = [0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12, 1, 6, 11]
+		res = ishift_row(data)
+		assert res == range(16)
 
 	def test_inverse_mix_columns(self):
-		pass
+		data = [2, 7, 0, 5, 6, 3, 4, 1, 10, 15, 8, 13, 14, 11, 12, 9]
+		res = imix_column(data)
+		assert res == range(16)
